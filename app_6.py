df357f8f305a945f5732b27766eac1252e503f3c
HaeIn2021 <85663393+HaeIn2021@users.noreply.github.com>
2023-03-14 20:23:17 +0900

    Add files via upload

---
+++ app_6.py
@@ -0,0 +1,6 @@
+!pip install spacy_streamlit
+import spacy_streamlit
+
+models = ["en_core_web_sm", "en_core_web_md"]
+default_text = "Sundar Pichai is the CEO of Google."
+spacy_streamlit.visualize(models, default_text)
