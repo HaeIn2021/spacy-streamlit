3454f3711700fc03def532a8163997f57dce3fa7
HaeIn2021 <85663393+HaeIn2021@users.noreply.github.com>
2023-03-14 19:59:41 +0900

    Add files via upload

---
+++ app_5.py
@@ -0,0 +1,7 @@
+import spacy
+import spacy_streamlit
+from spacy_streamlit import visualize_tokens
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Sundar Pichai is the CEO of Google.")
+visualize_tokens(doc, attrs=["text", "pos_", "dep_", "ent_type_"])
